0c3b0fcf4f45aede2f61a3910109f407a915e1d1
Mushtaq Ahmed <mushtaq@gmail.com>
2023-11-24 18:42:54 +0530

    day5a

--- src/main/scala/tc/implementation/Sorter.scala
+++ src/main/scala/tc/implementation/Sorter.scala
@@ -4,4 +4,15 @@ import tc.lib.Ord
 
 object Sorter {
 
+  def sort(xs: List[Int]): List[Int] = xs match {
+    case head :: tail => insert(head, sort(tail))
+    case Nil => Nil
+  }
+
+  def insert(x: Int, sortedList: List[Int]): List[Int] = sortedList match {
+    case head :: _ if x <= head => x :: sortedList
+    case head :: tail => head :: insert(x, tail)
+    case Nil => List(x)
+  }
+
 }
---
+++ worksheets/day5/42.sc
@@ -0,0 +1,4 @@
+import tc.implementation.Sorter
+
+Sorter.sort(List(3, 10, 32, 2, 8, 12))
+Sorter.sort(List("z", "abc", "a", "ss"))
